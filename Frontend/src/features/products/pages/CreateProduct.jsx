55a103f59819e92eadee4ad7433e99c643fa5a8f
Nayan <nayanbhusari02@gmail.com>
2026-04-17 19:05:40 +0530

    feat: add CreateProduct page and register its route in the application router

---
+++ Frontend/src/features/products/pages/CreateProduct.jsx
@@ -0,0 +1,12 @@
+import { useProduct } from "../hooks/useProduct";
+
+const CreateProduct = () => {
+
+  const { handleCreateProduct } = useProduct()
+  
+  return (
+    <div>CreateProduct</div>
+  )
+}
+
+export default CreateProduct;
