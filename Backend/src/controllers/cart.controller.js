8c520a2c63657c72038a1907fb03b6589ae930e4
Nayan <nayanbhusari02@gmail.com>
2026-04-21 18:15:23 +0530

    feat: implement addToCart functionality with product validation and cart management

--- Backend/src/controllers/cart.controller.js
+++ Backend/src/controllers/cart.controller.js
@@ -1,5 +1,35 @@
 import cartModel from "../models/cart.model.js";
+import productModel from "../models/product.model.js";
 
 export async function addToCart() {
+  const { productId, varientId } = req.params;
 
+  const product = await productModel.findOne({
+    _id: productId,
+    "varients._id": varientId,
+  });
+
+  if (!product) {
+    return res.status(404).json({
+      message: "product or varient not found",
+      success: false,
+    });
+  }
+
+  // find product in cart or not if not create/add in cart
+
+  const cart =
+    (await cartModel.findOne({ user: req.user._id })) ||
+    (await cartModel.create({ user: req.user._id })); 
+
+  // product or varient is in cart or not
+  const isProductAlreadyInCart = cart.item.some(
+    item.product.toString() === productId &&
+      item.varient?.toSting() === varientId,
+  );
+
+  // product quanitity increase by one if already in cart
+  if (isProductAlreadyInCart) {
+    
+  }
 }
