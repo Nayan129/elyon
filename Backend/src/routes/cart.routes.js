90c2c1dae398e3a2c8b6d7fe582335c046dada91
Nayan <nayanbhusari02@gmail.com>
2026-04-28 10:58:17 +0530

    feat: Implement order creation and payment verification with Razorpay integration

--- Backend/src/routes/cart.routes.js
+++ Backend/src/routes/cart.routes.js
@@ -6,12 +6,17 @@ import {
 } from "../validator/cart.validator.js";
 import {
   addToCart,
+  createOrderController,
   getCart,
   incrementCartItemQuantity,
+  verifyOrderController,
 } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
+/**
+ * @route POST /api/cart/add/:productId/:variantId
+ */
 router.post(
   "/add/:productId/:variantId",
   authenticateUser,
@@ -33,5 +38,12 @@ router.patch(
   validateIncrementCartItemQuantity,
   incrementCartItemQuantity,
 );
+
+/**
+ * @route POST /api/cart/payment/create/order
+ */
+router.post("/payment/create/order", authenticateUser, createOrderController);
+
+router.post("/payment/verify/order", authenticateUser, verifyOrderController);
 
 export default router;
